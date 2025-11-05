43be9a9e46f2c646b1af2248920db77d747d9a2c
martiolle <mart.90.develop@gmail.com>
2025-11-05 13:09:44 +0100

    delete end

--- app/src/main/java/com/chapter/android/nav3/navigation/Destinations.kt
+++ app/src/main/java/com/chapter/android/nav3/navigation/Destinations.kt
@@ -2,4 +2,3 @@ package com.chapter.android.nav3.navigation
 
 data object SplashPokemonDestination
 data object ListPokemonDestination
-data class DetailPokemonDestination(val id: Int)
--- app/src/main/java/com/chapter/android/nav3/navigation/NavGraph.kt
+++ app/src/main/java/com/chapter/android/nav3/navigation/NavGraph.kt
@@ -3,16 +3,10 @@ package com.chapter.android.nav3.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.entryProvider
-import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
-import com.chapter.android.nav3.presentation.detail.DetailScreen
-import com.chapter.android.nav3.presentation.detail.DetailViewModel
 import com.chapter.android.nav3.presentation.list.ListScreen
 import com.chapter.android.nav3.presentation.splash.SplashScreen
-import org.koin.androidx.compose.koinViewModel
-import org.koin.core.parameter.parametersOf
 
 @Composable
 fun NavGraph() {
@@ -22,10 +16,6 @@ fun NavGraph() {
     NavDisplay(
         backStack = backStack,
         onBack = { backStack.removeLastOrNull() },
-        entryDecorators = listOf(
-            rememberSaveableStateHolderNavEntryDecorator(),
-            rememberViewModelStoreNavEntryDecorator()
-        ),
         entryProvider = entryProvider {
             entry<SplashPokemonDestination> {
                 SplashScreen(
@@ -35,15 +25,6 @@ fun NavGraph() {
             entry<ListPokemonDestination> {
                 ListScreen(
                     onItem = { backStack.add(DetailPokemonDestination(it)) },
-                    onBack = { backStack.removeLastOrNull() }
-                )
-            }
-            entry<DetailPokemonDestination> {
-                val vm: DetailViewModel = koinViewModel(
-                    parameters = { parametersOf(it.id) }
-                )
-                DetailScreen(
-                    viewModel = vm,
                     onBack = { backStack.removeLastOrNull() }
                 )
             }
